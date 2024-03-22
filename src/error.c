2f69ec59f2b1633259a55639f508177d0d4a61ee
Abdelilah Mousaid <51842546+26TOXIC26@users.noreply.github.com>
2024-03-22 05:46:21 +0000

    Update error.c

--- src/error.c
+++ src/error.c
@@ -19,7 +19,7 @@ void	is_error(void)
 	ft_putstr_fd("---> In case you write julia make ", 2);
 	ft_putstr_fd("sure your next arg is must be numbers\n", 2);
 	ft_putstr_fd("				[EXMPLES]\n", 2);
-	ft_putstr_fd("        ./fractol mandlebrot\n", 2);
+	ft_putstr_fd("        ./fractol mandelbrot\n", 2);
 	ft_putstr_fd("        ./fractol julia 0.15 0.78\n", 2);
 	exit(0);
 }
